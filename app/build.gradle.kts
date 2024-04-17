8fc876f8e6efb36c815a4874b92ad3b0d1ce340e
ayushkalathia <ayushkalathia@gmail.com>
2024-04-17 09:10:08 +0530

    changes in expense_list.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
+    implementation ("androidx.drawerlayout:drawerlayout:1.1.1")
+    implementation ("com.google.android.material:material:1.5.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.activity:activity:1.8.2")
     testImplementation("junit:junit:4.13.2")
